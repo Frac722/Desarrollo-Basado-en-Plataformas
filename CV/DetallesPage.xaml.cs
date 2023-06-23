428a50a08f5cee8f1671f52244e58c2e04277a84
Alain Freiker Mullisaca Rivera <106561279+Frac722@users.noreply.github.com>
2023-06-23 03:19:22 -0500

    Add files via upload

---
+++ CV/DetallesPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CV
+{
+    public partial class DetallesPage : ContentPage
+    {
+        public DetallesPage(string nombre, DateTime fechaNacimiento, string ocupacion, string contacto, string nacionalidad, string nivelIngles, List<string> lenguajesProgramacion)
+        {
+            InitializeComponent();
+
+            // Mostrar los datos en los elementos de la página
+            nombreLabel.Text = nombre;
+            fechaNacimientoLabel.Text = fechaNacimiento.ToString("dd/MM/yyyy");
+            ocupacionLabel.Text = ocupacion;
+            contactoLabel.Text = contacto;
+            nacionalidadLabel.Text = nacionalidad;
+            nivelInglesLabel.Text = nivelIngles;
+            lenguajesProgramacionLabel.Text = string.Join(", ", lenguajesProgramacion);
+        }
+    }
+
+
+}
---
+++ CV/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CV
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void EnviarButton_Clicked(object sender, EventArgs e)
+        {
+            // Obtén los datos del formulario
+            string nombre = nombreEntry.Text;
+            DateTime fechaNacimiento = fechaNacimientoPicker.Date;
+            string ocupacion = ocupacionEntry.Text;
+            string contacto = contactoEntry.Text;
+            string nacionalidad = nacionalidadPicker.SelectedItem?.ToString();
+            string nivelIngles = GetSelectedRadioButtonText();
+            List<string> lenguajesProgramacion = GetSelectedCheckBoxes();
+
+            // Crea una nueva página y pasa los datos como parámetros
+            DetallesPage detallesPage = new DetallesPage(nombre, fechaNacimiento, ocupacion, contacto, nacionalidad, nivelIngles, lenguajesProgramacion);
+
+            // Realiza la navegación a la nueva página
+            await Navigation.PushAsync(detallesPage);
+        }
+
+        private string GetSelectedRadioButtonText()
+        {
+            if (basicoRadioButton.IsChecked)
+                return "Básico";
+            else if (intermedioRadioButton.IsChecked)
+                return "Intermedio";
+            else if (avanzadoRadioButton.IsChecked)
+                return "Avanzado";
+            else if (fluidoRadioButton.IsChecked)
+                return "Fluido";
+            else
+                return string.Empty;
+        }
+
+        private List<string> GetSelectedCheckBoxes()
+        {
+            List<string> selectedLenguajes = new List<string>();
+
+            if (javaCheckBox.IsChecked)
+                selectedLenguajes.Add("Java");
+            if (pythonCheckBox.IsChecked)
+                selectedLenguajes.Add("Python");
+            if (javascriptCheckBox.IsChecked)
+                selectedLenguajes.Add("JavaScript");
+            if (cplusCheckBox.IsChecked)
+                selectedLenguajes.Add("C++");
+            if (phpCheckBox.IsChecked)
+                selectedLenguajes.Add("PHP");
+
+            return selectedLenguajes;
+        }
+    }
+}
